423015e719cd2bad170201b30d075a2327864c58
Trinity Booth <trinitybooth89@gmail.com>
2025-01-31 00:03:53 +0000

    feat: Added REST API controller class to test MongoDB integration

---
+++ src/main/java/com/trinity/planit/service/UserService.java
@@ -0,0 +1,22 @@
+package com.trinity.planit.service;
+
+import com.trinity.planit.model.User;
+import com.trinity.planit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getUsersByName(String name) {
+        return userRepository.findByName(name);
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+}
