127555ece95250937721e687851f8a9da2009e34
Trinity Booth <trinitybooth89@gmail.com>
2025-02-03 18:14:12 +0000

    feat: added Event and Organisation model classes with same setup as Member

---
+++ src/main/java/com/trinity/planit/model/Event.java
@@ -0,0 +1,16 @@
+package com.trinity.planit.model;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "events")
+@JsonInclude(JsonInclude.Include.NON_NULL) // Pre
+public class Event {
+}
