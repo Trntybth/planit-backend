3313115fa215fb4dc2c8f8af29f966af93325e85
Trinity Booth <trinitybooth89@gmail.com>
2025-02-03 18:13:07 +0000

    feat: created Member model class with necessary annotations

---
+++ src/main/java/com/trinity/planit/model/Member.java
@@ -0,0 +1,17 @@
+package com.trinity.planit.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "members")
+@JsonInclude(JsonInclude.Include.NON_NULL) // Pre
+public class Member extends User{
+}
