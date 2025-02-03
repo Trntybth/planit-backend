0e617107d86c90f0c87645ab5b7f0c715a1fb2b3
Trinity Booth <trinitybooth89@gmail.com>
2025-02-03 18:56:35 +0000

    feat: added fields to Event class with @JsonProperty annotations

--- src/main/java/com/trinity/planit/model/Event.java
+++ src/main/java/com/trinity/planit/model/Event.java
@@ -1,10 +1,15 @@
 package com.trinity.planit.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,5 +18,24 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "events")
 @JsonInclude(JsonInclude.Include.NON_NULL) // Pre
 public class Event {
+    @Id
+    private String id;
 
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("location")
+    private String location;
+
+    @JsonProperty("creator")
+    private Organisation creator;
+
+    @JsonProperty("date")
+    private LocalDate date;
+
+    @JsonProperty("attendees")
+    private List<Member> attendees;
 }
